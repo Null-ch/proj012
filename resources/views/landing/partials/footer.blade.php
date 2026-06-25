54054f74df42190d6027b577e9739849fd1c408c
Null-ch <zuxobot@yandex.ru>
2026-06-25 18:48:53 +0300

    feat: added policy in footer

--- resources/views/landing/partials/footer.blade.php
+++ resources/views/landing/partials/footer.blade.php
@@ -4,17 +4,19 @@
             <h3>Контакты</h3>
             <p>{{ $contact['address'] }}</p>
             <p><a href="tel:{{ preg_replace('/\D+/', '', $contact['primary_phone']) }}">{{ $contact['primary_phone'] }}</a></p>
-            @if(isset($contact['email']))
-            <p><a href="mailto:{{ $contact['email'] }}">{{ $contact['email'] }}</a></p>
+            @if (isset($contact['email']))
+                <p><a href="mailto:{{ $contact['email'] }}">{{ $contact['email'] }}</a></p>
             @endif
         </div>
         <div class="footer__nav">
-            @foreach($navigation as $item)
+            @foreach ($navigation as $item)
                 @php
                     $href = $item['href'];
 
                     if ($href === '#contacts') {
                         $href = route('landing.contacts');
+                    } elseif ($href === '#privacy-policy') {
+                        $href = route('landing.privacy-policy');
                     } elseif (str_starts_with($href, '#') && request()->routeIs('landing.contacts')) {
                         $href = route('landing.home') . $href;
                     }
