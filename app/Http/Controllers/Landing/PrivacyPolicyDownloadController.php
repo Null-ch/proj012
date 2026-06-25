a43300db61afec6e89c93dedfac90c1f4e2eea8e
Null-ch <zuxobot@yandex.ru>
2026-06-25 18:36:30 +0300

    fix: правки страницы с политикой

--- app/Http/Controllers/Landing/PrivacyPolicyDownloadController.php
+++ app/Http/Controllers/Landing/PrivacyPolicyDownloadController.php
@@ -10,7 +10,7 @@ final class PrivacyPolicyDownloadController extends Controller
     public function __invoke(): BinaryFileResponse
     {
         return response()->download(
-            resource_path('content/privacy-policy.txt'),
+            resource_path('content/privacy-policy.docx'),
             'politika-obrabotki-personalnyh-dannyh.txt',
             ['Content-Type' => 'text/plain; charset=UTF-8'],
         );
